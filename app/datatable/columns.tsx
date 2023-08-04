2e682ab95af9a1c667bd1808582820582ab11fea
Cesar Murillo <namiayumis@gmail.com>
2023-08-04 17:08:46 +0900

    add 100 reecords by page

--- app/datatable/columns.tsx
+++ app/datatable/columns.tsx
@@ -1,23 +1,22 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef } from "@tanstack/react-table";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
 export type Contacts = {
-	id: string;
-    vocative: string;
-	name: string;
-	last_name: string;
-    organization: string;
-    title: string;
-	is_vip: boolean;
-	is_invited: boolean;
-    qr_code: string;	
-    invitation_date: string;
-}
-
+  id: string;
+  vocative: string;
+  name: string;
+  last_name: string;
+  organization: string;
+  title: string;
+  is_vip: boolean;
+  is_invited: boolean;
+  qr_code: string;
+  invitation_date: string;
+};
 
 export const columns: ColumnDef<Contacts>[] = [
   {
@@ -32,4 +31,4 @@ export const columns: ColumnDef<Contacts>[] = [
     accessorKey: "organization",
     header: "Organization",
   },
-]
+];
--- app/datatable/data-table.tsx
+++ app/datatable/data-table.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { Button } from "@/components/ui/button"
 import {
   ColumnDef,
   flexRender,
@@ -32,15 +32,23 @@ export function DataTable<TData, TValue>({
 }: DataTableProps<TData, TValue>) {
   const table = useReactTable({
     data,
-    columns,
+    columns,  
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+          pageSize: 100,
+      },
+    },
   })
 
   return (
+    <div>
     <div className="rounded-md border">
+    
       <Table>
         <TableHeader>
+         
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
@@ -82,5 +90,71 @@ export function DataTable<TData, TValue>({
         </TableBody>
       </Table>
     </div>
+    <div className="flex items-center gap-2">
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>>'}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+            className="border p-1 rounded w-16"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={e => {
+            table.setPageSize(Number(e.target.value))
+          }}
+        >
+          {[100, 200, 300, 400, 500].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>{table.getRowModel().rows.length} Rows</div>
+      <pre>{JSON.stringify(table.getState().pagination, null, 2)}</pre>
+
+    </div>
+    
   )
 }
