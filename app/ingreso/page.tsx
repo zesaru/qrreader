49c758f89f99f2e19ca0c0e447822eaeb3291506
Cesar Murillo <namiayumis@gmail.com>
2023-08-23 13:03:55 +0900

    add menu component at home

--- app/ingreso/page.tsx
+++ app/ingreso/page.tsx
@@ -1,15 +1,15 @@
+'use server'
+
 import {createServerComponentClient} from '@supabase/auth-helpers-nextjs'
 import {cookies} from 'next/headers'
 import {redirect} from 'next/navigation'
 import ContactsTables from '../../components/ui/contacts-table';
 import { type Database } from '../../types/database'
 
-
 export default async function Index() {
     const supabase = createServerComponentClient<Database>({cookies})
-    const {data:contacts } = await supabase.from('contacts').select('*').eq('is_entered', true).order('is_vip', { ascending: true })
-
-    
+    const {data:contacts } = await supabase.from('contacts').select('*').eq('is_entered', true).order('is_vip', { ascending: false })
+  
     const {
         data: { user },
       } = await supabase.auth.getUser()
@@ -21,8 +21,6 @@ export default async function Index() {
       }
 
     return (
-
-
         <main className='flex min-h-screen flex-col items-center justify-between p-2'>
             <ContactsTables contacts={contacts}/>
         </main>
--- components/ui/contacts-table.tsx
+++ components/ui/contacts-table.tsx
@@ -1,60 +1,82 @@
-import { type Contact } from '../../types/contacts';
+"use client";
 
-export default function ContactsTable( {contacts} : {contacts: Contact[] | null }) {
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { type Contact } from "../../types/contacts";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
+export default function ContactsTable({
+  contacts,
+}: {
+  contacts: Contact[] | null;
+}) {
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("realtime contacts")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "contacts",
+        },
+        () => {
+          router.refresh();
+
+          return () => {
+            supabase.removeChannel(channel);
+          };
+        }
+      )
+      .subscribe();
+  }, [supabase, router]);
   return (
     <>
-
-
-        
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Position
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-        {
-          contacts?.map(contact => {
-            return (
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th
-              scope="row"
-              className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              <div className="pl-1">
-                <div className="text-base font-semibold">{contact.vocative} {contact.name?.split(' ',1)} {contact.last_name?.split(' ',1)}</div>
-                <div className="font-normal text-gray-500">
-                  {contact.organization}
-                </div>
-              </div>
-            </th>
-            <td className="px-6 py-4">React Developer</td>
-            <td className="px-6 py-4">
-              <div className="flex items-center">
-                <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2" />{" "}
-                Online
-              </div>
-            </td>
-          </tr>
-          )
-          
-          }
-          )}
-        </tbody>
-      </table>
-    </div>
-
-      
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="text-xs text-gray-700 uppercase">
+          total {contacts?.filter((contact) => contact.is_vip).length}
+        </div>
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts?.map((contact) => {
+              return (
+                <tr
+                  key={contact.id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                >
+                  <th
+                    scope="row"
+                    className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    <div className="pl-1">
+                      <div className="flex items-center">
+                        <div className="font-semibold h-2.5 w-2.5 rounded-full bg-green-500"></div>
+                        <div className="text-base flex my-1 ">
+                          {contact.vocative} {contact.name?.split(" ", 1)} {contact.last_name?.split(" ", 1)}
+                        </div>
+                        <div className="text-base text-red-700">{contact.is_vip ? "VIP" : ""}</div>
+                      </div>
+                      <div className="font-normal text-gray-500 ">
+                        {contact.organization}
+                      </div>
+                    </div>
+                  </th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
