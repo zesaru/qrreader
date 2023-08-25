ae8077cdc8b4090e2c83416362df13fc35cf8662
Cesar Murillo <namiayumis@gmail.com>
2023-08-25 17:25:11 +0900

    uiqrclient

--- components/ui/qrclient.tsx
+++ components/ui/qrclient.tsx
@@ -1,76 +1,114 @@
-"use client"
+"use client";
 
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { QrScanner } from '@yudiel/react-qr-scanner';
-import { useState } from 'react';
-import { redirect } from 'next/navigation'
-import { Contact } from '../../types/contacts';
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { QrScanner } from "@yudiel/react-qr-scanner";
+import { useState } from "react";
+import { redirect } from "next/navigation";
+import { Contact } from "../../types/contacts";
+import Link from "next/link";
 
 async function fetchContacts(id: string) {
   const supabase = createClientComponentClient();
-  
-  if (!id || id === '' || id === undefined) {
-    throw new Error('Invalid id parameter');
+
+  if (!id || id === "" || id === undefined) {
+    throw new Error("Invalid id parameter");
   }
   try {
-    const data  = await supabase.from('contacts').select().eq('qr_code', id);
-    console.log(data);
-    return data ;
+    const { data } = await supabase.from("contacts").select().eq("qr_code", id);
+    return data;
   } catch (error) {
-
+    console.log(error);
     throw error;
   }
 }
-export default function ClientComponent<Contact>() {
-  const [contact , setContact] = useState([]);
+export default function ClientComponent() {
+  const [contact, setContact] = useState({} as Contact[]);
+  const [toglee, setToglee] = useState(true);
 
   const handleDecode = async (result: string) => {
     try {
-      const contact = await fetchContacts(result);
-      //const name = contacts![0].name as string | null;
-      //const last_name = contacts![0].last_name as string | null;
-      //const is_vip = contacts![0].is_vip as string | null;
-      //const contactName = name + ' ' + last_name;
-      //setContact(contacts![0]);
-      console.log('------------------------------')
-      console.log(contact)
-      
+      const contacts = await fetchContacts(result);
+      setContact(contacts as Contact[]);
+      setToglee(false);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleError = (error: { message: any; }) => {
+  const handleError = (error: { message: any }) => {
     console.log(error?.message);
   };
-
   const handleClick = () => {
-    setContact([]);
-    redirect('/todos');
+    setContact({} as Contact[]);
+    setToglee(true);
+    redirect("/test");
   };
 
   return (
-  <>
-    <div className='w-full lg:w-1/2'>
-    <h1 className='text-4xl text-center'>Scan QR Code</h1>
-    <QrScanner
-      onDecode={handleDecode}
-      onError={handleError}
-    />
-    </div>
-   
-   {contact === undefined  && <> Poker Face</>}
-   
-   <div>
-    <p className='text-2xl p-4'>Name: </p>
-      <button className='bg-green-600 p-4 white font-bold text-white' onClick={handleClick}>Back</button>
-   </div>
-    
-    <div>
-      <p className='text-2xl p-4'>No contact found</p>
-    </div>
-    
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+      <Link
+        href="/"
+        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+        >
+          <polyline points="15 18 9 12 15 6" />
+        </svg>{" "}
+        Back
+      </Link>
+      <div></div>
+      {toglee && (
+        <>
+          <div className="w-full lg:w-1/2">
+            <h1 className="text-4xl text-center">Scan QR Code</h1>
+            <QrScanner onDecode={handleDecode} onError={handleError} />
+          </div>
+          <div>
+            <p className="text-2xl p-4"> </p>
+            <button
+              className="bg-green-600 p-4 white font-bold text-white"
+              onClick={handleClick}
+            >
+              Back
+            </button>
+          </div>
+        </>
+      )}
 
-  </>
+      {contact.length === 0 && (
+        <>
+          <div>
+            <p className="text-2xl p-4">Invitado no encontrado</p>
+          </div>
+          <div>
+            <p className="text-2xl p-4"> </p>
+            <button
+              className="bg-green-600 p-4 white font-bold text-white"
+              onClick={handleClick}
+            >
+              Back
+            </button>
+          </div>
+        </>
+      )}
+      {contact.length > 0 && (
+        <div>
+          <p className="text-xl">{contact[0].vocative} {contact[0].name} {contact[0].last_name}</p>
+          {contact[0].is_vip && <span className="text-xl bg-green-600 text-white text-center p-1">VIP</span> }
+          <p className="text-xl"> {contact[0].organization}</p>
+          <p className="text-xl"> {contact[0].title}</p>
+        </div>
+      )}
+    </div>
   );
 }
